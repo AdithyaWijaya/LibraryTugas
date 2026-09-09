dba17f7a5cdc98414e64597c2a93a495a801fcbc
Adithya Wijaya <adithyawijayajr@gmail.com>
2026-09-09 16:00:37 +0800

    Add initial implementation of helloWorld and formatEXAM programs; update .gitignore to include all .exe files

--- formatEXAM.c
+++ formatEXAM.c
@@ -1,6 +1,21 @@
+// Volume Kerucut
+
 #include <stdio.h>
 
 int main () {
     printf("I Gede Adithya Wijaya, XI.4, 6\n");
     printf("===============================\n");
+
+    float r, t, v;
+
+    printf("-Algoritma menghitung Volume Kerucut-\n");
+    printf("Masukan jari-jari: ");
+    scanf("%f", &r);
+    printf("Masukan tinggi: ");
+    scanf("%f", &t);
+
+    v=(1.0/3.0)*3.14*r*r*t;
+
+    printf("Volume Kerucut adalah: %.2f", v);
+    return 0;
 }
